3ad51485b79094ffce9b90eea3ea6e8426bd0ac5
thayllon-py <thayllonryan17@gmail.com>
2021-09-29 10:32:37 -0300

    Documentação

--- staticfiles/modal.js
+++ staticfiles/modal.js
@@ -2,18 +2,26 @@ $(document).ready(function () {
     var time = $('#recipient-name')
     var estado =  $('#estado')
     var bool_button = false
+    /* Vai receber o evento submit e usa ro e.preventDefault para nao ocorrer o evento */
     $('#form1').on('submit',function(e) {
         e.preventDefault();
+        /* Pega o valor atual da quantidade por pagina, isso porque quando for renderizar o html com o ajax
+            e nao ignorar a quantidade selecionada anteriomente 
+        */
         filter_table = $('select[id=filter_table] option').filter(':selected').val()
+        /*  desc_or_asc =$('#invert').val()
+            pesq = $('#pesquisa').val()
+            Mesma coisa que acontece com o filter table    
+        */
         desc_or_asc =$('#invert').val()
         pesq = $('#pesquisa').val()
+
+        /* O ajax, evia tudo que pegamos anteriomente via post, vai vai enviar os dados pra fazer as query */
         $.ajax({
             url: 'inc/filter.php',
             type: "POST",
             data: {'search':pesq, 'desc_or_asc': desc_or_asc,'filterObj':filter_table},
             datType:'JSON',
-            beforeSend: function(){
-            },
             success: function(res){
                 $('.filter_data').html(res)
             }
@@ -38,21 +46,29 @@ $(document).ready(function () {
     $('button[name=success]').addClass('btn-times btn-light')
     
     time.bind('keyup',function(e) {
+        /* Evento keyup para capturar as teclas digitadas, e convefere se tem o valor
+            se não, vai dar para a variavel false, se tiver, True e dar uma classe que nao vai deixar
+            o button clicavel
+        */
         if(time.val()){
             bool_button = true
         }else{
             bool_button=false
             $('button[name=success]').addClass('btn-times btn-light')
         }
+        /* Mesmo esquema, se tiver selecionado o primeiro (que é o option selected) vai retornar false */
         if(estado.val() == $("#estado option:first").val()){
             bool_button = false
             $('button[name=success]').addClass('btn-times btn-light')
         }
+        /* se for true retorna uam classe que deixa clicavel e tira as classes que impossibilita */
         if(bool_button){
             $('button[name=success]').removeClass('btn-times btn-light')
             $('button[name=success]').addClass('btn-success')
         }
     });
+
+    /* Mesmo esquema do de cima */
     
     estado.on('change', function () {
         if(estado.val() == $("#estado option:first").val()){
@@ -73,6 +89,19 @@ $(document).ready(function () {
     });
 
 
+    /*
+    
+    O submit cancelado do modal, vai enviar todos os dados necessarios para nao serem ignorado e quebrarem o layout
+    
+    Quando for enviado o modal vai ser fechado com o beforeSend do ajax, e quando success, vai renderizar a responta com .html()
+
+    Vai resetar os campos do modal, o select optione o input
+
+    e o sucesso() vai mostrar um sweetalert para mostrar sucesso
+
+    e colocando as classes para deixar inclicavel
+
+    */
     $('#add_fut').on('submit',function(e) {
         e.preventDefault();
         filter_table = $('select[id=filter_table] option').filter(':selected').val()
--- staticfiles/script.js
+++ staticfiles/script.js
@@ -1,5 +1,10 @@
+/* Nesse arquivo vai ser explicado oque nao foi explicado no outro */
+
 $(document).ready(function () {
     var desc_or_asc =$('#invert').val()
+    /* Vai vai pegar o value do button que ta como ASC como padra, se clicar via mudar pra desc e enviar via post
+    o resto ja foi explicado no outro arquivo js
+    */
     $('#invert_seq').on('click', function(){
         if(desc_or_asc == 'DESC'){
             desc_or_asc = 'ASC'
@@ -36,13 +41,19 @@ $(document).ready(function () {
         })
 
     })
+
+    /* O botão de deletar vai pegar o id e o nome do time
+        que foi disponibilizado no php.
+    */
     $('.delete_time').on('click', function() {
         pesq = $('#pesquisa').val()
         desc_or_asc =$('#invert').val()
         filter_table = $('select[id=filter_table] option').filter(':selected').val()
         var id = $(this).attr("id");
         var time = $(this).attr("name")
+        /* Abrimos um swal para perguntar se o usuario tem certeza */
         Swal.fire({
+            /* Icone de cuidado */
             icon: 'warning',
             title: '<strong>Exclusão de time</strong>',
             
@@ -56,6 +67,7 @@ $(document).ready(function () {
             cancelButtonText:
                 'Cancelar',
         }).then((value) => {
+            /* Aqui pegamos o valor, e verificamos se o isConfirmed está como True */
             if(value.isConfirmed){
 
                 $.ajax({
@@ -66,6 +78,7 @@ $(document).ready(function () {
                     success: function(res){
                         $('.filter_data').html(res)
                         Swal.fire({
+                            /* Enviamos tudo que tem de enviar e mostramos a tela de confirmação da exclusão, assim como na criação */
                             icon: 'success',
                             title:'O time '+time+' foi apagado',
                             showConfirmButton: false,
